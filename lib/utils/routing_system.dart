6e21afe7f511f0da4b10d229cd5c64750232b6c3
MatheusBorelli <73211294+MatheusBorelli@users.noreply.github.com>
2023-10-18 01:21:23 -0300

    Adding more info to error page

--- lib/utils/routing_system.dart
+++ lib/utils/routing_system.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ifsp_inventariado/models/item.dart';
 import 'package:ifsp_inventariado/models/salas.dart';
+import 'package:ifsp_inventariado/views/item_detail.dart';
 
 import 'package:ifsp_inventariado/views/lista_salas.dart';
 import 'package:ifsp_inventariado/views/user_page.dart';
@@ -13,37 +14,38 @@ class RoutingSystem {
     switch(settings.name){
       case '/':
         return MaterialPageRoute(builder: (_) => const UserPage());
+     
       case '/salas':
         return MaterialPageRoute(builder: (_) => const ListaSalas());
+
       case '/barreader':
         if(args is Sala){
-          return MaterialPageRoute(
-            builder: (_) => BarCodePage(
-              data: args,
-            )
-          );
+          return MaterialPageRoute(builder: (_) => BarCodePage(data: args));
         }
+
         return _errorRoute();
       case '/itemdetail':
         //Must implement
         if(args is Item){
-          return _errorRoute();  
+          return MaterialPageRoute(builder: (_) => ItemDetail(data: args));  
         }
-        return _errorRoute();
+        
+        return _errorRoute(args: args);
 
       default:
-        return _errorRoute();
+        return _errorRoute(args: args);
     }
   }
 
-  static Route<dynamic> _errorRoute(){
+  static Route<dynamic> _errorRoute({args}){
     return MaterialPageRoute(builder: (_) {
       return Scaffold(
         appBar: AppBar(
-          title: const Text("Erro"),
+          title: const Text("Error"),
+          centerTitle: true,
         ),
-        body: const Center(
-          child: Text("Not Found")
+        body: Center(
+          child: Text(args.toString())
         ),
       );
     });
